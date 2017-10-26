53914f677c015f2745baa3c11c74121285e11c1a
Imran Khan <m_imran_khan@me.com>
2017-10-26 16:46:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// var moment = require('moment'); // template string
+import moment from 'moment';       // import statement
+
+console.log("Hello from JavaScript!");
+// moment
+console.log(moment().startOf('day').fromNow());
+
+var name ="Imran", time = "today";
+console.log(`Hi ${name}, how are you ${time}?`);
